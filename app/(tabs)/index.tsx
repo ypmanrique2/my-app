1b0e02df79314b0fdeb677aa2de47b8feaba8370
Yadin Paulo Manrique Marquez <ypmanrique2@misena.edu.co>
2024-10-01 19:23:06 -0500

    npx expo install expo-linear-gradient

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello world </ThemedText>
+        <ThemedText type="title">Hello world-</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
